2f0b1a32bc054b2caf8e757e78589176ce23dd7f
Joy Das <35962760+joydas65@users.noreply.github.com>
2019-11-22 14:32:13 +0530

    Create JULKA_Julka.py

---
+++ JULKA_Julka.py
@@ -0,0 +1,9 @@
+for _ in range(10):
+    
+    apples = int(input())
+    
+    more_apples_with_klaudia = int(input())
+    
+    print(apples - ((apples - more_apples_with_klaudia)//2))
+    
+    print((apples - more_apples_with_klaudia)//2)
